98ab1c4293480e74a7b94bf5df4f8c3a4d1f0d3b
waitlight <waitlight@yeah.net>
2025-05-10 10:42:47 +0800

    delete engine

--- src/test/java/org/waitlight/simple/jsonql/engine/DeleteEngineTest.java
+++ src/test/java/org/waitlight/simple/jsonql/engine/DeleteEngineTest.java
@@ -6,8 +6,6 @@ import org.waitlight.simple.jsonql.entity.Blog;
 import org.waitlight.simple.jsonql.entity.User;
 import org.waitlight.simple.jsonql.metadata.MetadataSources;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,44 +35,14 @@ public class DeleteEngineTest {
                     ]
                 }
                 """.formatted(randomName);
-        Object insertResult = engine.execute(insertQuery);
-        assertNotNull(insertResult);
+        Object insertResultObj = engine.execute(insertQuery);
+        assertNotNull(insertResultObj);
 
-        Long insertedId;
-        if (insertResult instanceof InsertExecutionResult) {
-            InsertExecutionResult result = (InsertExecutionResult) insertResult;
-            assertTrue(result.getAffectedRows() > 0, "Insert should affect at least one row");
-            assertFalse(result.getMainIds().isEmpty(), "Insert should return at least one ID");
-            insertedId = result.getMainIds().get(0);
-        } else {
-            // 兼容旧版返回值类型
-            assertTrue(insertResult instanceof Number && ((Number) insertResult).intValue() > 0,
-                    "Insert should affect at least one row");
+        InsertExecutionResult insertResult = (InsertExecutionResult) insertResultObj;
+        assertTrue(insertResult.getAffectedRows() > 0, "Insert should affect at least one row");
+        assertFalse(insertResult.getMainIds().isEmpty(), "Insert should return at least one ID");
 
-            // 通过查询获取ID
-            String selectQueryForId = """
-                    {
-                        "statement": "query",
-                        "select": ["id"],
-                        "from": "user",
-                        "where": {
-                            "type": "comparison",
-                            "field": "name",
-                            "operator": "eq",
-                            "value": "%s"
-                        }
-                    }
-                    """.formatted(randomName);
-
-            Object selectResult = engine.execute(selectQueryForId);
-            assertNotNull(selectResult, "Select result for ID should not be null");
-            assertTrue(selectResult instanceof List, "Select result should be a list");
-            @SuppressWarnings("unchecked")
-            List<Map<String, Object>> resultList = (List<Map<String, Object>>) selectResult;
-            assertFalse(resultList.isEmpty(), "Inserted record should be found to get its ID");
-            insertedId = Long.parseLong(resultList.get(0).get("id").toString());
-        }
-        
+        Long insertedId = insertResult.getMainIds().get(0);
         assertNotNull(insertedId, "Inserted ID should not be null");
 
         String deleteQuery = """
